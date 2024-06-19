23e1ac0d87fdf4a45e50341d6dc38a39f0af06f2
dboybkru <48415715+dboybkru@users.noreply.github.com>
2024-06-19 21:21:59 +0200

    1

--- Django/rent_project/rent_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Apartment
-
-class ApartmentForm(forms.ModelForm):
-    class Meta:
-        model = Apartment
-        fields = ['title', 'description', 'price', 'address']
